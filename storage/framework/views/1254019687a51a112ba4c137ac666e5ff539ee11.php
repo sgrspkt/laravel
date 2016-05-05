e0d13da3b08876d88a1acdce8f2431e097ff8ba7
rabin <rabin@gmail.com>
2016-05-05 15:16:30 +0545

    fetched completed

---
+++ storage/framework/views/1254019687a51a112ba4c137ac666e5ff539ee11.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>title</title>
+</head>
+<body>
+
+<div class="container">
+<div class="row">
+	
+	<?php echo $__env->yieldContent('content'); ?>;
+</div>
+</div>
+</body>
+</html>
+
